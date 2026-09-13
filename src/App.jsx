64a57ca5962febee5cfae2f9a6b202c0105f8273
CristhianMateo <mateosant7@gmail.com>
2026-09-13 16:04:48 -0500

    Tablero Kanban de TaskFlow conectado a la API real, con login y ruta protegida

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
